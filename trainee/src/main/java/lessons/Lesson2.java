37d23b22616ac0213f37068f18561e62a0763692
FilyanovichNP <nata.filyanovich@gmail.com>
2023-11-09 22:08:21 +0300

    create printThreeWords() method

---
+++ trainee/src/main/java/lessons/Lesson2.java
@@ -0,0 +1,14 @@
+package lessons;
+
+public class Lesson2 {
+    /*  Создаи?те метод printThreeWords(),
+     * которыи? при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.*/
+    public static void printThreeWords() {
+        String s1 = "Orange", s2 = "Banana", s3 = "Apple";
+        System.out.println(s1 + "\n" + s2 + "\n" + s3);
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+}
