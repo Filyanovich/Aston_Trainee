44903bd43b3296e70924295a2950c42d58e2d2fc
FilyanovichNP <nata.filyanovich@gmail.com>
2023-11-11 17:37:51 +0300

    creat task 1

--- trainee/src/main/java/lessons/Lesson3.java
+++ trainee/src/main/java/lessons/Lesson3.java
@@ -1,7 +1,14 @@
 package lessons;
 
 public class Lesson3 {
-    public static void main(String[] args) {
 
+    /*1.Ќаписать метод, принимающий на вход два целых числа и провер€ющий что их сумма лежит в пределах от 10 до 20
+     (включительно), если да Ц вернуть true, в противном случае Ц false.*/
+    public static boolean chekSumRange(int a, int b){
+        boolean sum= a + b >= 10 && a + b <= 20;
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(chekSumRange(5,5));
     }
 }
